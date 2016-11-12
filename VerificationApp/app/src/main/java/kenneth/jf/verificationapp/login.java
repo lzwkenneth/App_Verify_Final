1c83b352208810faf295331957471ef0eba9044b
jinfa <chuajinfa@gmail.com>
2016-11-12 17:45:20 +0800

    commit

--- VerificationApp/app/src/main/java/kenneth/jf/verificationapp/login.java
+++ VerificationApp/app/src/main/java/kenneth/jf/verificationapp/login.java
@@ -113,7 +113,7 @@ public class login extends AppCompatActivity {
             //SET ADDRESS OF THE SERVER
             //ConnectionInformation.getInstance().setUrl("192.168.43.244:8443");
             //ConnectionInformation.getInstance().setUrl("192.168.1.10:8443");
-            ConnectionInformation.getInstance().setUrl("192.168.0.103:8443");
+            ConnectionInformation.getInstance().setUrl("192.168.43.153:8443");
             //ConnectionInformation.getInstance().setUrl("192.168.56.1:8443");
             restTemplate = restTemplate2;
         } catch (Exception e) {
